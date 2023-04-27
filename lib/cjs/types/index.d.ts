2e678fb6c1468b7da009ac592510ada4c115753c
Dylan Hall <dylan@perseeventures.com>
2023-04-27 12:35:52 -0500

    added builds

---
+++ lib/cjs/types/index.d.ts
@@ -0,0 +1,16 @@
+export declare function helloWorld(): string;
+export declare function goodBye(): string;
+export declare function getEnvironmentVariables(): void;
+export declare function getStripeClientAccountCountry(): Promise<string>;
+export declare function getPlaidLinkToken(): Promise<string>;
+export declare function sendPostmarkEmail(): Promise<string>;
+declare const _default: {
+    helloWorld: typeof helloWorld;
+    goodBye: typeof goodBye;
+    getEnvironmentVariables: typeof getEnvironmentVariables;
+    getPlaidLinkToken: typeof getPlaidLinkToken;
+    getStripeClientAccountCountry: typeof getStripeClientAccountCountry;
+    sendPostmarkEmail: typeof sendPostmarkEmail;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
