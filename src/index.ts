12e8b6b9e5ce6ce7da79f4d953e85b9cdcc4e594
Dylan Hall <dylan@perseeventures.com>
2023-04-27 11:35:32 -0500

    tests for stripe, plaid, and postmark

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import {
+  Configuration,
+  CountryCode,
+  PlaidApi,
+  PlaidEnvironments,
+  Products,
+} from "plaid";
+import { ServerClient } from "postmark";
+import Stripe from "stripe";
+
 export function helloWorld() {
   const message = "Hello World from my example modern npm package!";
   return message;
@@ -8,7 +18,101 @@ export function goodBye() {
   return message;
 }
 
+export function getEnvironmentVariables() {
+  console.log(`STRIPE_PRIVATE_KEY: ${process.env.STRIPE_PRIVATE_KEY}`);
+  console.log(`PLAID_CLIENT_ID: ${process.env.PLAID_CLIENT_ID}`);
+  console.log(`PLAID_SECRET: ${process.env.PLAID_SECRET}`);
+  console.log(`POSTMARK_API: ${process.env.POSTMARK_API}`);
+}
+
+export async function getStripeClientAccountCountry(): Promise<string> {
+  if (!process.env.STRIPE_PRIVATE_KEY) {
+    throw new Error("STRIPE_PRIVATE_KEY environment variable is not defined");
+  }
+  const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY, {
+    apiVersion: "2022-11-15",
+  });
+
+  const accounts = await stripe.accounts.retrieve();
+  if (!accounts?.country) {
+    throw new Error("Unable to obtain Stripe Client Account Country");
+  }
+
+  return accounts.country;
+}
+
+export async function getPlaidLinkToken(): Promise<string> {
+  if (!process.env.PLAID_CLIENT_ID) {
+    throw new Error("PLAID_CLIENT_ID environment variable is not defined");
+  }
+
+  if (!process.env.PLAID_SECRET) {
+    throw new Error("PLAID_SECRET environment variable is not defined");
+  }
+
+  const configuration = new Configuration({
+    basePath: PlaidEnvironments.production,
+    baseOptions: {
+      headers: {
+        "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+        "PLAID-SECRET": process.env.PLAID_SECRET,
+      },
+    },
+  });
+
+  const client = new PlaidApi(configuration);
+  const test = await client.linkTokenCreate({
+    client_name: "Safe Rate",
+    country_codes: [CountryCode.Us],
+    language: "en",
+    products: [Products.Auth],
+    user: {
+      client_user_id: "07816aff-4bb4-407c-81e9-04afd4cd5022",
+    },
+    redirect_uri: "https://saferate.com/account",
+  });
+
+  if (!test?.data?.link_token) {
+    throw new Error("Unable to obtain Plaid Link Token");
+  }
+
+  return test.data.link_token;
+}
+
+export async function sendPostmarkEmail(): Promise<string> {
+  if (!process.env.POSTMARK_API) {
+    throw new Error("POSTMARK_API environment variable is not defined");
+  }
+
+  const postmarkClient = new ServerClient(process.env.POSTMARK_API);
+  const result = await postmarkClient.sendEmailWithTemplate({
+    From: "team@saferate.com",
+    To: "dylan@saferate.com",
+    TemplateAlias: "to-agent-saver-linked-to-agent",
+    TemplateId: 28144270,
+    TemplateModel: {
+      agentFirstName: "Agent first name",
+      borrowerFirstName: "Borrower first name",
+      borrowerLastName: "Borrower last name",
+      loginUrl: "https://www.google.com",
+      saverAgentCredits: "$1,000",
+      toEmail: "dylan@saferate.com",
+      toName: "Dylan Hall",
+    },
+  });
+
+  if (!result?.MessageID) {
+    throw new Error("Unable to send Postmark email");
+  }
+
+  return result.MessageID;
+}
+
 export default {
   helloWorld,
   goodBye,
+  getEnvironmentVariables,
+  getPlaidLinkToken,
+  getStripeClientAccountCountry,
+  sendPostmarkEmail,
 };
--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,11 +1,42 @@
 import { expect, test, describe, beforeAll } from "vitest";
 import sql, { Database } from "@leafac/sqlite";
+import {
+  getEnvironmentVariables,
+  getPlaidLinkToken,
+  getStripeClientAccountCountry,
+  sendPostmarkEmail,
+} from "../src";
 
 test("Math.sqrt()", () => {
   expect(Math.sqrt(4)).toBe(2);
   expect(Math.sqrt(144)).toBe(12);
   expect(Math.sqrt(2)).toBe(Math.SQRT2);
   expect(Math.sqrt(1)).toBe(1);
+});
+
+test("Environment Variables", () => {
+  expect(process.env.STRIPE_PRIVATE_KEY).toBeDefined();
+  expect(process.env.PLAID_CLIENT_ID).toBeDefined();
+  expect(process.env.PLAID_SECRET).toBeDefined();
+  expect(process.env.POSTMARK_API).toBeDefined();
+  getEnvironmentVariables();
+});
+
+test("Plaid Link Token", async () => {
+  const linkToken = await getPlaidLinkToken();
+  console.log(`Link Token: ${linkToken}`);
+  expect(linkToken).toBeDefined();
+});
+
+test("Stripe", async () => {
+  const stripeCountry = await getStripeClientAccountCountry();
+  expect(stripeCountry).toBeDefined();
+});
+
+test("Postmark", async () => {
+  // const messageId = await sendPostmarkEmail();
+  // console.log(messageId);
+  // expect(messageId).toBeDefined();
 });
 
 describe("Servicing Database", () => {
